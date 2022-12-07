bd20d115569400bb6e4489cd064f9ff6a24d5d67
amcaw <42608053+amcaw@users.noreply.github.com>
2022-12-07 10:23:19 +0100

    Create ENTSOE.py

---
+++ ENTSOE.py
@@ -0,0 +1,18 @@
+import os
+
+import pandas as pd
+from entsoe import EntsoePandasClient
+
+import pandas as pd
+from entsoe import EntsoePandasClient
+
+# %% parameter definitions
+client = EntsoePandasClient(api_key=os.environ['api_key'])
+
+start = pd.Timestamp('20220101', tz ='UTC')
+end = pd.Timestamp('20221231', tz ='UTC')
+country_code = 'BE'  # 
+
+day_ahead_prices_BE = client.query_day_ahead_prices(country_code, start=start, end=end)
+
+day_ahead_prices_BE.to_csv('./outfile.csv')
