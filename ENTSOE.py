b166eeb25757d627241f7265acc7ac68707af0bc
amcaw <42608053+amcaw@users.noreply.github.com>
2022-12-13 14:55:30 +0100

    Update ENTSOE.py

--- ENTSOE.py
+++ ENTSOE.py
@@ -17,7 +17,11 @@ day_ahead_prices_BE = client.query_day_ahead_prices(country_code, start=start, e
 
 df = day_ahead_prices_BE.to_frame('Value')
 
+df.index.names = ['Date']
+
 df = df.resample("M", origin="2001-01-01 00:00:00+01:00",label="left").mean()
+
+df['Value'] = df['Value'].round(2)
 
 df['date_jour'] = dt.datetime.today().strftime("%e/%m/%Y")
 
