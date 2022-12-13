4ed66ca4006f25ba234b6c3c588261bb890bac74
amcaw <42608053+amcaw@users.noreply.github.com>
2022-12-13 14:34:43 +0100

    Update ENTSOE.py

--- ENTSOE.py
+++ ENTSOE.py
@@ -12,4 +12,10 @@ country_code = 'BE'  #
 
 day_ahead_prices_BE = client.query_day_ahead_prices(country_code, start=start, end=end)
 
-day_ahead_prices_BE.to_csv('./outfile.csv')
+df = day_ahead_prices_BE.to_frame('Value')
+
+belpex_daily_avg = df.resample("D", origin="2001-01-01 00:00:00+01:00",label="left").mean()
+
+belpex_monthly_avg = belpex_daily_avg['Value'].resample('M').mean()
+
+belpex_monthly_avg.to_csv('./BE_day.csv')
