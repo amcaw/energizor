6ca3bde9a1cdb6476ccbf4c1c99e78a629b1da27
amcaw <42608053+amcaw@users.noreply.github.com>
2022-12-14 11:00:36 +0100

    Update endex.py

--- endex.py
+++ endex.py
@@ -4,10 +4,10 @@ df = pd.read_csv("https://guillaumederval.github.io/endexHistory/data.csv").set_
 
 #ENDEX 103
 df2 = df.loc["2022-12":]
-df2 = df2[(df2["product"] == "Q1-23") & (df2["market"] == "BPB")]
+df3 = df2[(df2["product"] == "Q1-23") & (df2["market"] == "BPB")]
 
-df2.to_csv('endex_histo.csv')
+df3.to_csv('endex_histo.csv')
 
-df2 = df2[(df2["product"] == "Q1-23") & (df2["market"] == "BPB")].value.mean()
+df4 = df2[(df2["product"] == "Q1-23") & (df2["market"] == "BPB")].value.mean()
 
-df2.to_csv('endex_mean.csv')
+df4.to_csv('endex_mean.csv')
