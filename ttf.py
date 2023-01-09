275798b156888d1be637c0f894b2ed910f6bed6c
amcaw <42608053+amcaw@users.noreply.github.com>
2023-01-09 15:28:08 +0100

    Update ttf.py

--- ttf.py
+++ ttf.py
@@ -7,6 +7,6 @@ df2 = df.loc["2022-12":]
 
 df4 = df2[(df2["product"] == "FEB-23") & (df2["market"] == "TFM")].value.mean().round(2)
 
-df4 = pd.DataFrame([['endex',df4]], columns=['name', 'value'])
+df4 = pd.DataFrame([['endex',df4]], columns=['name', 'Value'])
 
 df4.to_csv('gaz_mean.csv')
