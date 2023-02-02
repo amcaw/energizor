41be376e8438188a14a59926520e4ade26ea043b
amcaw <42608053+amcaw@users.noreply.github.com>
2023-02-02 10:33:35 +0100

    Update ttf.py

--- ttf.py
+++ ttf.py
@@ -1,19 +1,18 @@
 import pandas as pd
 
-df = pd.read_csv("https://guillaumederval.github.io/endexHistory/data.csv").set_index("date")
+#df = pd.read_csv("https://guillaumederval.github.io/endexHistory/data.csv").set_index("date")
 
 #TTF 101 histo
 
-df3 = df[(df["product"] == "MAR-23") & (df["market"] == "TFM")]
+#df3 = df[(df["product"] == "MAR-23") & (df["market"] == "TFM")]
 
-df3.to_csv('ttf_histo.csv')
+#df3.to_csv('ttf_histo.csv')
 
 #TTF 101 mean
 
-df2 = df.loc["2023-02":]
+df2 = df2.loc["2023-02":]
+df2 = df2.reset_index()
+df2 = df2[(df2["product"] == "MAR-23") & (df2["market"] == "TFM")].value.mean()
+df2 = pd.DataFrame([['ttf_101',df2]], columns=['name', 'Value'])
 
-df4 = df2[(df2["product"] == "MAR-23") & (df2["market"] == "TFM")].value.mean().round(2)
-
-df4 = pd.DataFrame([['ttf_101',df4]], columns=['name', 'Value'])
-
-df4.to_csv('gaz_mean.csv')
+df2.to_csv('gaz_mean.csv')
