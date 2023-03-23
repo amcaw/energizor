9a7a6d90106e74fc30ab21b34a489a327affc93d
amcaw <42608053+amcaw@users.noreply.github.com>
2023-03-23 10:47:42 +0100

    Update ttf_mean.py

--- ttf_mean.py
+++ ttf_mean.py
@@ -3,9 +3,10 @@ df2 = pd.read_csv("https://guillaumederval.github.io/endexHistory/data.csv").set
 
 
 #TTF 101
-df2 = df2.loc["2023-03":]
+df2 = df2.loc["2023-02":]
+#df2 = df2.loc["2023-03":]
 df2 = df2.reset_index()
-#df2 = df2[(df2["product"] == "JAN-23") & (df2["market"] == "TFM")].value.mean()
-df2 = df2[(df2["product"] == "APR-23") & (df2["market"] == "TFM")].value.mean()
+df2 = df2[(df2["product"] == "MAR-23") & (df2["market"] == "TFM")].value.mean()
+#df2 = df2[(df2["product"] == "APR-23") & (df2["market"] == "TFM")].value.mean()
 df2 = pd.DataFrame([['ttf_101',df2]], columns=['name', 'Value']).round(2)
 df2.to_csv('gaz_mean.csv')
